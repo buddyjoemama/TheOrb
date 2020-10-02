7d9e93d28e8935d1d9a54b6bc9faec7059ceb364
Brian Bacon <brian.bacon01@gmail.com>
2020-10-02 16:18:23 -0400

    Added some new effects

---
+++ Assets/TheOrbGame/Scripts/SpawnProjecle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjecle : MonoBehaviour
+{
+    public GameObject firePoint;
+    public GameObject vfx;
+
+    private GameObject effectToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnFire()
+    {
+        Instantiate(vfx, firePoint.transform.position, Quaternion.identity);
+    }
+
+    void SpawnVFX()
+    {
+
+    }
+}
