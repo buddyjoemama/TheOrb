99a1255c9307669f49e7f91dfd5aed57ea3fec22
Brian Bacon <brian.bacon01@gmail.com>
2020-11-25 15:50:48 -0500

    Updated scene

--- Assets/TheOrbGame/Scripts/Camera/CameraMovement.cs
+++ Assets/TheOrbGame/Scripts/Camera/CameraMovement.cs
@@ -22,15 +22,26 @@ public class CameraMovement : MonoBehaviour
         var mousePos = InputSystem.GetDevice<Mouse>().position;
 
         Ray ray = mainCamera.ScreenPointToRay(new Vector3(mousePos.x.ReadValue(), mousePos.y.ReadValue()));
-        Debug.DrawRay(ray.origin, ray.direction * 500, Color.yellow);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.yellow);
+        Debug.DrawRay(Vector3.zero, ray.direction * 500, Color.blue);
+        Debug.Log(ray.direction);
+        Vector3 lookAt = Vector3.zero;
 
-        if(Physics.Raycast(ray, out RaycastHit hit, 1000) && hit.collider.tag == "Floor")
+        if(Physics.Raycast(ray, out RaycastHit hit, 1000))
         {
-            //        gunRig.transform.Rotate(new Vector3(0, lookVector.y * 4), Space.Self);
-            playerManager.rig.transform.LookAt(hit.point);
+            lookAt = hit.point;
+        }
+        else
+        {
+            // pythag.
+            //Mathf.Sqrt(transform.position.y^2, )
+
+            lookAt = ray.GetPoint(1000);
         }
 
-        Debug.Log(hit.collider);
+        playerManager.rig.transform.LookAt(lookAt);
+
+        Debug.DrawLine(ray.origin, lookAt, Color.red);
     }
 
     private void LateUpdate()
